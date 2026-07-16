65d4d30d941bd3690f4bab220b2794c300814848
Saud Alqawsi <saudalqawsi@gmail.com>
2026-07-16 04:58:44 +0000

    File changes

--- src/components/navigation/PerimeterNav.jsx
+++ src/components/navigation/PerimeterNav.jsx
@@ -40,7 +40,7 @@ export default function PerimeterNav() {
       >
         <div className="flex items-center justify-between px-6 md:px-12 py-5">
           {/* Logo */}
-          <TnasuqLogo size="md" onClick={() => scrollTo('hero')} dark={scrolled} />
+          <TnasuqLogo size="md" onClick={() => scrollTo('hero')} />
 
           {/* Desktop nav */}
           <div className="hidden md:flex items-center gap-10">
